c97e8e58b3860341d0cea705984a02f4a307e9ae
Danial Aghaei <daniyal.aghai82@gmail.com>
2026-05-31 19:29:47 +0330

    Add Vercel on my Prokect

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <>
-      <p className="bg-red-500 text-white inline-block m-6 px-4 py-2 rounded-md cursor-pointer">Hello World!</p>
+      <p className="bg-red-500 text-white inline-block m-6 px-4 py-2 rounded-md cursor-pointer">Hello World! Add Project to Vercel</p>
     </>
   );
 }
