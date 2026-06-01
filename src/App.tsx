03b24b04d01f8a7970c2815ec1370095e0be3e50
Danial Aghaei <daniyal.aghai82@gmail.com>
2026-06-01 11:45:21 +0330

    Add Layout to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Layout from "./components/layout";
 
 function App() {
   return (
     <>
-      <p className="bg-red-500 text-white inline-block m-6 px-4 py-2 rounded-md cursor-pointer">Hello World! Add Project to Vercel</p>
+      <Layout>
+        <div>Hello</div>
+      </Layout>
     </>
   );
 }
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,14 @@
+import type { PropsWithChildren } from "react";
+import Footer from "./footer";
+import Header from "./header";
+const Layout: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
